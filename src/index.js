617cca45e292ed4f401e425f3500c5e7235d9787
Yslamguly <islamguly28@gmail.com>
2022-12-29 21:22:36 +0100

    wrapped ShoppingCartContext to share global state

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import AuthProvider from "./hooks/UserContext";
+import ShoppingCartContext from "./hooks/ShoppingCartContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <AuthProvider>
-            <App/>
+            <ShoppingCartContext>
+                <App/>
+            </ShoppingCartContext>
         </AuthProvider>
     </React.StrictMode>
 );
