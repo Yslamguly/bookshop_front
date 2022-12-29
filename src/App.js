118391d729423f68b505560d1f705ca2a563142b
Yslamguly <islamguly28@gmail.com>
2022-12-29 21:17:53 +0100

    toggle ShoppingCart.js on icon click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import {Routess} from './Routes';
-
+import {useShoppingCart} from "./hooks/ShoppingCartContext";
+import {ShoppingCart} from "./pages/ShoppingCart";
+import {useAuth} from "./hooks/UserContext";
 
 export const App = () => {
+    const {showShoppingCart} = useShoppingCart()
+    const {userId} = useAuth()
     return (
             <div className="page-container">
+                {showShoppingCart && <ShoppingCart userId={userId}/>}
                 <Routess/>
             </div>
     );
