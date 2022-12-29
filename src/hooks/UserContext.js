07f05b1699f388fc01e45d04cabdb55a200ea164
Yslamguly <islamguly28@gmail.com>
2022-12-29 21:26:52 +0100

    added userId to share it as a global state

--- src/hooks/UserContext.js
+++ src/hooks/UserContext.js
@@ -1,9 +1,11 @@
 import React  from "react";
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext({
     auth: null,
     setAuth: () => {},
+    userId:null,
+    setUserId:() => {},
     rememberMe:false,
     setRememberMe: () =>{}
 });
@@ -13,8 +15,9 @@ export const useAuth = () => useContext(AuthContext);
 const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState(sessionStorage.getItem("token") || localStorage.getItem("token"));
     const [rememberMe,setRememberMe] = useState(!!localStorage.getItem("token"))
+    const [userId,setUserId] = useState(null)
     return (
-        <AuthContext.Provider value={{ auth, setAuth,rememberMe,setRememberMe }}>
+        <AuthContext.Provider value={{ auth, setAuth,rememberMe,setRememberMe,userId,setUserId }}>
             {children}
         </AuthContext.Provider>
     );
