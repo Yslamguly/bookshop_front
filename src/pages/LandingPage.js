9072eb7a051030fb345666132ac8efe939212215
Yslamguly <islamguly28@gmail.com>
2023-03-03 10:11:07 +0100

    added landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,94 @@
+export default function LandingPage() {
+    return (
+        <div className="relative overflow-hidden bg-white">
+            <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+                <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+                    <div className="sm:max-w-lg">
+                        <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                            Explore the World of Books
+                        </h1>
+                        <p className="mt-4 text-xl text-gray-500">
+                            Discover a wide variety of books in our collection. From bestsellers to rare finds, we have it all.
+                        </p>
+                    </div>
+                    <div>
+                        <div className="mt-10">
+                            {/* Decorative image grid */}
+                            <div
+                                aria-hidden="true"
+                                className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+                            >
+                                <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                                    <div className="flex items-center space-x-6 lg:space-x-8">
+                                        <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                                                <img
+                                                    src="https://www.databazeknih.cz/img/books/44_/448480/big_rozpravky-poema-ruslan-a-ludmila-whj-448480.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://m.media-amazon.com/images/I/51PcUAhn15L._SX498_BO1,204,203,200_.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://m.media-amazon.com/images/I/511sfrMrIWL._AC_SY780_.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://i.pinimg.com/originals/cb/81/23/cb81237f40c0b0140fe9a529217d3736.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://m.media-amazon.com/images/I/419rlyQg8PL.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://images.penguinrandomhouse.com/cover/9780307831972"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
+                                                <img
+                                                    src="https://m.media-amazon.com/images/I/41Bci2pDRvL._AC_SY780_.jpg"
+                                                    alt=""
+                                                    className="h-full w-full object-cover object-center"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <a
+                                href="/books"
+                                className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700"
+                            >
+                                Start shopping
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
