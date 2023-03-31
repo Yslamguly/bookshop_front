35476a80d93a4946b1b1940a6b4127ba75a85dba
Yslamguly <islamguly28@gmail.com>
2023-03-31 14:15:24 +0200

    feat<signin>: redirect to google signin page on sign in with Google button click

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import {useToken} from "../hooks/auth/useToken";
 import logo from '../assets/inkwellBook.svg'
@@ -13,8 +13,20 @@ export default function SignIn() {
     const [showError, setShowError] = useState(false)
     const [emailValue, setEmailValue] = useState('')
     const [passwordValue, setPasswordValue] = useState('')
+    const [googleOauthUrl , setGoogleOauthUrl] = useState()
+
     const navigate = useNavigate();
     const {setAuth, setRememberMe, rememberMe} = useAuth();
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/auth/google/url')
+            .then((response)=>{
+                const {url} = response.data
+                setGoogleOauthUrl(url)
+            })
+            .catch((e)=>console.error(e))
+    },[])
+
     const onRememberMeClicked = () => {
         setRememberMe(!rememberMe)
         setIsRememberMeChecked(!rememberMe)
@@ -107,7 +119,7 @@ export default function SignIn() {
                                 </label>
                             </div>
                             <div className="text-sm">
-                                <a href={'/forgot_password'}
+                                <a href={'/forgot-password'}
                                    className="font-medium text-indigo-600 hover:text-indigo-500">
                                     Forgot your password? |
                                 </a>
@@ -124,8 +136,27 @@ export default function SignIn() {
                                 type={"submit"}
                                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                </span>Sign in
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                </span>Sign in
+                            </button>
+                            <div
+                                className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                                <p
+                                    className="mx-4 mb-0 text-center font-semibold dark:text-neutral-700">
+                                    OR
+                                </p>
+                            </div>
+                            <button type="button"
+                                    disabled={!googleOauthUrl}
+                                    onClick={()=>{window.location.href = googleOauthUrl}}
+                                    className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                                <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false"
+                                     data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 488 512">
+                                    <path fill="currentColor"
+                                          d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                                </svg>
+                                Sign in with Google
                             </button>
                         </div>
                     </div>
