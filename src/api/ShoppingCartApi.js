0e65f758f0bfee1336b4081c64cba21606c30f83
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-05-24 11:47:46 +0200

    move onQuantityChange api call to another file

--- src/api/ShoppingCartApi.js
+++ src/api/ShoppingCartApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export function addBookToShoppingCart(product,userId,token) {
-    return axios.post(`http://localhost:8000/shopping_cart/addBook/${userId}`, {
+export const addBookToShoppingCart = async(product,userId,token) => {
+    return await axios.post(`http://localhost:8000/shopping_cart/addBook/${userId}`, {
         book_id: product.id,
         price: product.selling_price
     }, {
@@ -9,8 +9,24 @@ export function addBookToShoppingCart(product,userId,token) {
     })
 }
 
-export const fetchUserShoppingCart = (userId,token) =>{
-    return axios.get(`http://localhost:8000/shopping_cart/${userId}`,{
+export const fetchUserShoppingCart = async (userId,token) => {
+    return await axios.get(`http://localhost:8000/shopping_cart/${userId}`,{
+        headers: {authorization: `Bearer ${token}`}
+    })
+}
+export const deleteBookFromUserShoppingCart = async (userId,token,bookId) =>{
+    return await axios.delete(`http://localhost:8000/shopping_cart/deleteBook/${userId}`, {
+        data: {shopping_cart_item_book_id: bookId},
+        headers: {authorization: `Bearer ${token}`}
+    })
+}
+
+export const updateBookQuantity = async (quantity, bookId, price,userId,token) => {
+    return await axios.patch(`http://localhost:8000/shopping_cart/updateQuantity/${userId}`, {
+        book_id: bookId,
+        quantity: quantity,
+        total_price: quantity * price
+    }, {
         headers: {authorization: `Bearer ${token}`}
     })
 }
